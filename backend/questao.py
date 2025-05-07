e7abcfca27793f593f563f0d709fe932efaab345
Carolina <carolinalansoni@gmail.com>
2025-05-07 17:41:20 -0300

    inserir opção do professor mudar as questões

--- backend/questao.py
+++ backend/questao.py
@@ -31,3 +31,15 @@ def buscar_questao_por_id(id_questao):
     conn.close()
     return questao
 
+# Função para atualizar uma questão existente
+def atualizar_questao(id_questao, novo_enunciado, novaA, novaB, novaC, novaD, nova_resposta, nova_dica, novo_valor, novo_nivel):
+    conn = sqlite3.connect('quizDB.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+        UPDATE questoes
+        SET enunciado = ?, alternativaA = ?, alternativaB = ?, alternativaC = ?, alternativaD = ?,
+            resposta_correta = ?, dica = ?, valor = ?, nivel_dificuldade = ?
+        WHERE ID_questao = ?
+    """, (novo_enunciado, novaA, novaB, novaC, novaD, nova_resposta, nova_dica, novo_valor, novo_nivel, id_questao))
+    conn.commit()
+    conn.close()
